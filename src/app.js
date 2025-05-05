f4af1ffb25f3bad2f903dbead418aa30c90c3639
ramraj1157 <ramarajuboya1096@gmail.com>
2025-05-05 16:50:52 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,8 +13,6 @@ app.use(cookieParser());
 
 const routes = [
   "http://localhost:5173",
-  "http://localhost:5174",
-  "http://localhost:5175",
   "https://resume-generator-silk.vercel.app"
 ];
 const corsOptions = {
